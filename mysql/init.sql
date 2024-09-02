a02f0899730f32281e299db3177fd28378a6ef8e
Ilya Gaffarov <ilya.gaffarov@yandex.ru>
2024-09-02 14:54:54 +0300

    v.002m

--- mysql/init.sql
+++ mysql/init.sql
@@ -4,7 +4,7 @@ CREATE DATABASE IF NOT EXISTS practice_db;
 -- Выбираем базу данных для работы
 USE practice_db;
 
--- Создание таблицы для хранения проектов
+-- Создание таблицы для хранения пользователей
 CREATE TABLE IF NOT EXISTS users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     user_login VARCHAR(255) NOT NULL,
