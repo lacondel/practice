0e0f48be2428fc335dab3f3c840fa3d16811e958
lacondel <143336027+lacondel@users.noreply.github.com>
2024-10-25 13:41:16 +0300

    v.007m

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,10 +1,7 @@
--- Создаем базу данных, если она не существует
 CREATE DATABASE IF NOT EXISTS practice_db;
 
--- Выбираем базу данных для работы
 USE practice_db;
 
--- Создание таблицы для хранения пользователей
 CREATE TABLE IF NOT EXISTS users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     user_login VARCHAR(255) NOT NULL,
